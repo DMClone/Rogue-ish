1bbc9cf98879724354c8ee6387e8873d7dd24cbe
DMClone <230381@student.glu.nl>
2024-12-24 02:28:28 +0100

    Proper player aiming

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,14 @@
+using System;
+using Unity.Mathematics;
 using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public static PlayerController instance;
+    [SerializeField] private GameObject _gun;
+
+
     public Vector3 mousePos;
     public float lookingAngle;
 
@@ -26,6 +31,8 @@ public class PlayerController : MonoBehaviour
     {
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = transform.position.z;
+        _gun.transform.up = mousePos - _gun.transform.position;
+        _gun.transform.eulerAngles = new Vector3(_gun.transform.eulerAngles.x, 0, _gun.transform.eulerAngles.z);
     }
 
     void FixedUpdate()
@@ -38,10 +45,5 @@ public class PlayerController : MonoBehaviour
         Vector2 _direction = ownPos - (Vector2)transform.position;
         float _calculatedAngle = Vector2.Angle(Vector2.up, _direction);
         return _calculatedAngle;
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.DrawRay(transform.position, mousePos);
     }
 }
